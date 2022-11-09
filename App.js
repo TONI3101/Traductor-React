3cfc012db82a7f79090151f6154386bb6d9e1298
Toni3101 <antonioha3101@gmail.com>
2022-11-09 18:42:34 +0100

    firs try bitch

---
+++ App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+import  i18n  from 'i18next';
+
+const lngs = {
+  en: { nativeName: 'English'},
+  es: { nativeName: 'Español'}
+};
+
+function App() {
+  const { t } = useTranslation(); 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button type='submit' key={lng} onClick={()=> i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>{lngs[lng].nativeName}</button>
+          ))}
+        </div>
+        <p>
+          <Trans i18nKey={'description'}>
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('Learn')} 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
